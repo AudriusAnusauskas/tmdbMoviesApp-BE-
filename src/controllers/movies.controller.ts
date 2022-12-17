f3b88ccfb95eafa5134af0acbd02b835eb2e0507
Audrius Ausauskas <aaaudrius@gmail.com>
2022-12-17 17:31:43 +0200

    whis-16 Integrate TMDB to Retrieve List of Movies

--- src/controllers/movies.controller.ts
+++ src/controllers/movies.controller.ts
@@ -1,9 +1,27 @@
 import express from 'express';
-import movies from '../../tempData/movies';
+import axios from 'axios';
 import detailedMovie from '../../tempData/movie';
+import { convertMovie } from '../converters/movie.converter';
 
-const getMovies = (_req: express.Request, res: express.Response): void => {
-  res.json(movies);
+const getMovies = async (_req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> => {
+  const getTmdbMovies = async (): Promise<Movies> => {
+    const { data } = await axios.get<TmdbMovies>(
+      `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=1&vote_count.gte=1000&api_key=${process.env.API_KEY}`,
+    );
+    const movies: Movie[] = data.results.map(convertMovie);
+
+    return {
+      page: 1,
+      movies,
+      totalPages: data.total_pages,
+    };
+  };
+
+  try {
+    res.json(await getTmdbMovies());
+  } catch (err) {
+    next(err);
+  }
 };
 
 const getMovieDetails = (req: express.Request, res: express.Response): void => {
