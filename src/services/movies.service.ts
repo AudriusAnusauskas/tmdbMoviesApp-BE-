9c80830ff48d2018be149c6ead236528c06dd97a
Audrius Ausauskas <aaaudrius@gmail.com>
2022-12-19 22:52:43 +0200

    whis-17 implement movies cache, correct json files

--- src/services/movies.service.ts
+++ src/services/movies.service.ts
@@ -1,16 +1,23 @@
 import axios from 'axios';
 import { convertMovie } from '../converters/movie.converter';
 
+let moviesCache: Movie[] | undefined;
+let totalPagesCache: number | undefined;
+
 const getTmdbMovies = async (): Promise<Movies> => {
-  const { data } = await axios.get<TmdbMovies>(
-    `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=1&vote_count.gte=1000&api_key=${process.env.API_KEY}`,
-  );
-  const movies: Movie[] = data.results.map(convertMovie);
+  if (!moviesCache) {
+    const { data } = await axios.get<TmdbMovies>(
+      `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=1&vote_count.gte=1000&api_key=${process.env.API_KEY}`,
+    );
+
+    moviesCache = data.results.map(convertMovie);
+    totalPagesCache = data.total_pages;
+  }
 
   return {
     page: 1,
-    movies,
-    totalPages: data.total_pages,
+    movies: moviesCache || [],
+    totalPages: totalPagesCache || 1,
   };
 };
 
