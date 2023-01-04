e5b8d0c2774e61cc05a9d4e4398ed5b231b09607
Audrius Ausauskas <aaaudrius@gmail.com>
2023-01-04 19:33:28 +0200

    whis-21 implement pagination in backend

--- src/controllers/movies.controller.ts
+++ src/controllers/movies.controller.ts
@@ -1,9 +1,23 @@
 import express from 'express';
 import { getTmdbMovies, getTmdbMovieDetails } from '../services/movies.service';
 
-const getMovies = async (_req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> => {
+const getMovies = async (req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> => {
   try {
-    res.json(await getTmdbMovies());
+    const page = req.query.page as string | undefined;
+    let pageNumber: number | undefined;
+    if (typeof page === 'string') {
+      pageNumber = parseInt(page, 10);
+      if (isNaN(pageNumber)) {
+        pageNumber = 1;
+      }
+    } else if (typeof page === 'undefined') {
+      pageNumber = 1;
+    }
+    if (typeof pageNumber !== 'undefined' && pageNumber > 0) {
+      res.json(await getTmdbMovies(pageNumber));
+    } else {
+      res.sendStatus(400);
+    }
   } catch (err) {
     next(err);
   }
