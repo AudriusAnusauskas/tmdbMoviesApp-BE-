a6067eb5104a1506028d9bbf650d7c377963f6fd
Audrius Ausauskas <aaaudrius@gmail.com>
2023-01-05 18:43:38 +0200

    whis-21 refactor getMovies in movie controller

--- src/controllers/movies.controller.ts
+++ src/controllers/movies.controller.ts
@@ -3,21 +3,8 @@ import { getTmdbMovies, getTmdbMovieDetails } from '../services/movies.service';
 
 const getMovies = async (req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> => {
   try {
-    const page = req.query.page as string | undefined;
-    let pageNumber: number | undefined;
-    if (typeof page === 'string') {
-      pageNumber = parseInt(page, 10);
-      if (isNaN(pageNumber)) {
-        pageNumber = 1;
-      }
-    } else if (typeof page === 'undefined') {
-      pageNumber = 1;
-    }
-    if (typeof pageNumber !== 'undefined' && pageNumber > 0) {
-      res.json(await getTmdbMovies(pageNumber));
-    } else {
-      res.sendStatus(400);
-    }
+    const page = req.query.page ? parseInt(req.query.page as string) : 1;
+    res.json(await getTmdbMovies(page));
   } catch (err) {
     next(err);
   }
