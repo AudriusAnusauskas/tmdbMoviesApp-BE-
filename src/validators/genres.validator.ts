1cf86db69ec8b1770c5b6a14b87e1c66e82ffc82
Audrius Ausauskas <aaaudrius@gmail.com>
2023-01-16 11:11:28 +0200

    whis-25 update movie.service, validator

--- src/validators/genres.validator.ts
+++ src/validators/genres.validator.ts
@@ -2,11 +2,11 @@ const validateGenre = (genres: string[]) => {
   if (!Array.isArray(genres)) {
     return 'genres should be an array';
   }
-  for (const genre of genres) {
-    if (isNaN(genre)) {
-      return 'All genres should be integers';
-    }
-  }
+  // for (const genre of genres) {
+  //   if (isNaN(genre)) {
+  //     return 'All genres should be integers';
+  //   }
+  // }
   return true;
 };
 
