43e2515856604c9c43e8cb25294ed29ad9a40291
Audrius Ausauskas <aaaudrius@gmail.com>
2023-01-19 20:09:52 +0200

    validator update

--- src/validators/sort-option.validator.ts
+++ src/validators/sort-option.validator.ts
@@ -1,14 +1,8 @@
 import { getSortOptions } from '../services/sort-option.service';
 
+const validOptions = getSortOptions().map((e) => e.code);
 const validateSortOptions = (sort: string) => {
-  if (sort === undefined || sort === null || sort === '') {
-    return true;
-  }
-  const validOptions = getSortOptions();
-  if (validOptions.some((e) => e.code === sort)) {
-    return true;
-  }
-  return false;
+  return !sort || validOptions.includes(sort);
 };
 
 export default validateSortOptions;
