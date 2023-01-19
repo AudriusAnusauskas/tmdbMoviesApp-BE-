33c4df39d2e230c1bfc6c2d7c413b5bf1a3cb923
Audrius Ausauskas <aaaudrius@gmail.com>
2023-01-19 16:55:41 +0200

    upd validator w/ null, undefined, ''  & test w/''

--- src/validators/sort-option.validator.ts
+++ src/validators/sort-option.validator.ts
@@ -1,6 +1,9 @@
 import { getSortOptions } from '../services/sort-option.service';
 
 const validateSortOptions = (sort: string) => {
+  if (sort === undefined || sort === null || sort === '') {
+    return true;
+  }
   const validOptions = getSortOptions();
   if (validOptions.some((e) => e.code === sort)) {
     return true;
--- test/sort-optionValidator.test.ts
+++ test/sort-optionValidator.test.ts
@@ -4,6 +4,9 @@ describe('sort-options validator', () => {
   it('should return true if passed option is in the list of options returned by sort-option.service', () => {
     expect(validateSortOptions('vote_average.desc')).toBe(true);
   });
+  it('should return true if passed empty string', () => {
+    expect(validateSortOptions('')).toBe(true);
+  });
   it('should return false if passed any other value', () => {
     expect(validateSortOptions('some value')).toBe(false);
   });
