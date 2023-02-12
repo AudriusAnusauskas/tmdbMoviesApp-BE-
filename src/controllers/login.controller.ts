0a59db36a41c284a5f77837a398f4f11fe36ce44
Audrius Ausauskas <aaaudrius@gmail.com>
2023-02-12 12:10:25 +0200

    corrections after comments

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -14,23 +14,22 @@ const logInControl = async (req: express.Request, res: express.Response) => {
   try {
     const user = await login(email as string, password as string);
     if (!user) {
-      return res.status(401).json({ error: 'Wrong email or password 123' });
+      return res.status(401).json({ error: 'Wrong email or password' });
     }
 
     const getSecret = (): string => {
       const secret = process.env.ACCESS_TOKEN_SECRET;
       if (secret === undefined) {
-        throw Error;
+        throw new Error();
       }
       return secret;
     };
 
-    const token = jwt.sign({ data: email }, getSecret(), { expiresIn: String(process.env.JWT_EXPIRE) });
-    console.log(token);
+    jwt.sign({ data: email }, getSecret(), { expiresIn: String(process.env.JWT_EXPIRE) });
 
     return res.status(200).json({ token: 'value' });
   } catch (error) {
-    res.status(500).json({ error: (error as Error).message });
+    res.status(500).json({ error: error as Error });
   }
 };
 
--- src/validators/login.validator.ts
+++ src/validators/login.validator.ts
@@ -6,15 +6,6 @@ const validateLogin = [
     .withMessage('Email is a required field.')
     .isEmail()
     .withMessage('Email must be a valid email address.'),
-  body('password')
-    .notEmpty()
-    .withMessage('Password is a required field.')
-    .isLength({ min: 8 })
-    .withMessage('Password must be at least 8 characters.')
-    .matches(/^(?=.*[A-Z])(?=.*[0-9])(?=.*[!#$%^&*-_+])/)
-    .withMessage(
-      'Password must have at least one upper case letter, one number, and one special character (!#$%^&*-_+)',
-    ),
 ];
 
 export { validateLogin };
